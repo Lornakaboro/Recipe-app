6e06ece542c12f0709e6f48ac12781d77424547f
Lornakaboro <lornakaboro@gmail.com>
2023-09-14 17:34:45 +0300

    Generate views for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
+  get 'recipes/create'
+  get 'recipes/update'
+  get 'recipes/destroy'
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/edit'
+  get 'recipe_foods/destroy'
   resources :foods
   devise_for :users
   resources :users, only: [:index, :show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_071905) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_082904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
+  create_table "food_recipes", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "foods_id", null: false
+    t.decimal "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["foods_id"], name: "index_food_recipes_on_foods_id"
+    t.index ["recipe_id"], name: "index_food_recipes_on_recipe_id"
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string "food"
     t.string "measurement_unit"
-    t.float "unit_price"
+    t.decimal "unit_price", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_071905) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "food_recipes", "foods", column: "foods_id"
+  add_foreign_key "food_recipes", "recipes"
+  add_foreign_key "recipes", "users"
 end
