b1413661710b5c5f1e525cf359973fca6e976db2
Lornakaboro <lornakaboro@gmail.com>
2023-09-15 19:12:04 +0300

    Add controller for public-recipes

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,10 @@
 class Food < ApplicationRecord
+    class Food < ApplicationRecord
+        belongs_to :user
+        has_many :food_recipes
+        validates :name, presence: true
+        validates :measurement_unit, presence: true
+        validates :price, numericality: {}
+        validates :quantity, numericality: { only_integer: true }
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
-  get 'public_recipes/index'
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/edit'
+  get 'recipe_foods/destroy'
   resources :foods
   resources :recipes
+  resources :public_recipes
   devise_for :users
   resources :users, only: [:index, :show]
   root to: "users#index"
